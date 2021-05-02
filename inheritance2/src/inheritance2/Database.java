935e226ce9143240bdc8cb197ab4c118977b6787
murat-PC <murat-PC@DESKTOP-BT7G4N7>
2021-05-02 20:26:11 +0300

    Ders 3 Kodları

---
+++ inheritance2/src/inheritance2/Database.java
@@ -0,0 +1,11 @@
+package inheritance2;
+
+public class Database extends Logger{
+	
+	
+	
+	@Override
+	public void log() {
+		System.out.println("Veriler loglandý.");
+	}
+}
---
+++ inheritance2/src/inheritance2/File.java
@@ -0,0 +1,9 @@
+package inheritance2;
+
+public class File extends Logger{
+	
+	@Override
+	public void log() {
+		System.out.println("Dosyalar loglandý.");
+	}
+}
